71c44ffa3bdec9000e9496671866a4c417662ae1
juniel1299 <youngs5440@gmail.com>
2025-01-25 22:22:14 +0900

    소개 화면 기본적 내용 정리

--- common.js
+++ common.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
-    $('#home-area').click();
-    $(document).bind('keydown',function(e){
-        if(e.keyCode ==123){
-            e.preventDefault();
-            e.returnValue = false;
-        }
-    })
+    // $(document).bind('keydown',function(e){
+    //     if(e.keyCode ==123){
+    //         e.preventDefault();
+    //         e.returnValue = false;
+    //     }
+    // })
 });
 
 $('.header-button').click(function () {
@@ -19,11 +18,11 @@ $('.header-button').click(function () {
 });
 
 
-document.onmousedown=disableclick;
-function disableclick(event){
-    if (event.button==2) {
-        const status="우클릭 방지";
-        alert(status);
-        return false;
-    }
-}
+// document.onmousedown=disableclick;
+// function disableclick(event){
+//     if (event.button==2) {
+//         const status="우클릭 방지";
+//         alert(status);
+//         return false;
+//     }
+// }
