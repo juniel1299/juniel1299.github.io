c8e7619537dee5e137dd70d07578063b35b6536e
juniel1299 <youngs5440@gmail.com>
2025-02-04 21:06:46 +0900

    css 수정

--- common.js
+++ common.js
@@ -1,40 +1,21 @@
 $(document).ready(function () {
-    // $(document).bind('keydown',function(e){
-    //     if(e.keyCode ==123){
-    //         e.preventDefault();
-    //         e.returnValue = false;
-    //     }
-    // })
+    // 헤더 버튼 클릭 시 섹션 전환
+    $('.header-button').click(function () {
+        $('.header-button').removeClass('active');
+        $(this).addClass('active');
+
+        let id = $(this).data("target");
+
+        $('.main-area').fadeOut(200, function () {
+            $("#" + id).fadeIn(200);
+        });
+    });
+
+    // 상세 버튼 클릭 시 내용 표시
+    $('.detail-button').click(function () {
+        let id = $(this).data("target");
+
+        $(this).toggleClass('active');
+        $("#" + id).slideToggle(300);
+    });
 });
-
-$('.header-button').click(function () {
-    $('.header-button').removeClass('active');
-    $(this).addClass('active');
-    id = $(this).data("target");
-
-    $('.main-area').hide();
-    $("#"+id).show();
-
-});
-
-$('.detail-button').click(function () {
-    $('.detail-button').toggleClass('active');
-    id = $(this).data("target");
-
-    if(!$('#' + id).hasClass('active')){
-        $('#' + id).addClass('active')
-    }
-    else
-    {
-        $('#' + id).removeClass('active')
-    }
-});
-
-// document.onmousedown=disableclick;
-// function disableclick(event){
-//     if (event.button==2) {
-//         const status="우클릭 방지";
-//         alert(status);
-//         return false;
-//     }
-// }
